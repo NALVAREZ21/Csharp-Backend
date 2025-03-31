712cf1c133cbfd158cb4f53cdcb7f05128c021a4
ViviProgram <vivianalorenadiaz95@gmail.com>
2025-03-31 11:45:33 -0300

    Repasando Logica

---
+++ 00_repasoc#/Program.cs
@@ -0,0 +1,45 @@
+﻿// Console.WriteLine("Buen día");
+
+// Tipos de Datos
+// int number=15;
+// Console.WriteLine(number.GetType());
+// float real=10.5f;
+// Console.WriteLine(real.GetType());
+// double real_doble=15.8f;
+// Console.WriteLine(real_doble);
+// bool estado=false;
+// Console.WriteLine(estado.GetType());
+// string parrafo="Repasando Laboratorio";
+// Console.WriteLine(parrafo);
+// char letra='z';
+// Console.WriteLine(letra);//
+
+// // Estructura Selectiva
+// Console.Write("Ingrese su nombre: ");
+// string Nombre=Console.ReadLine();
+// if(Nombre=="Jose"){
+//    Console.WriteLine("Bienvenido"+ Nombre);
+// }
+// else if (Nombre!="Jose")
+// {
+// Console.WriteLine("Acceso denegado");
+// }
+//Estructura de Repetición
+/*
+for(int i=0;i<11;i++){
+    Console.WriteLine(i);
+}
+*/
+/*
+int i=20;
+while(i<31)
+{
+    
+    Console.WriteLine(i);
+    i++;
+}*/
+int i=40;
+do{
+Console.WriteLine(i);
+i++;
+} while(i<51);
