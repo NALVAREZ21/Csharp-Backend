7eab77934716683d491006e17b2d47464f310a8a
ViviProgram <vivianalorenadiaz95@gmail.com>
2025-04-02 11:53:29 -0300

    Actualizando repaso

--- 00_repasoc#/Program.cs
+++ 00_repasoc#/Program.cs
@@ -1,33 +1,30 @@
 ﻿// Console.WriteLine("Buen día");
 
-/*Tipos de Datos
-int number=15;
-Console.WriteLine(number.GetType());
-float real=10.5f;
-Console.WriteLine(real.GetType());
-double real_doble=15.8f;
-Console.WriteLine(real_doble);
-bool estado=false;
-Console.WriteLine(estado.GetType());
-string parrafo="Repasando Laboratorio";
-Console.WriteLine(parrafo);
-char letra='z';
-Console.WriteLine(letra);*/
-/*
-var nombre="Maria";
-Console.WriteLine(nombre);
-// Estructura Selectiva
-Console.Write("Ingrese su nombre: ");
-string Nombre=Console.ReadLine();
-if(Nombre=="Jose"){
-   Console.WriteLine("Bienvenido"+ Nombre);
-}
-else if (Nombre!="Jose")
-{
-Console.WriteLine("Acceso denegado");
-}
+// Tipos de Datos
+// int number=15;
+// Console.WriteLine(number.GetType());
+// float real=10.5f;
+// Console.WriteLine(real.GetType());
+// double real_doble=15.8f;
+// Console.WriteLine(real_doble);
+// bool estado=false;
+// Console.WriteLine(estado.GetType());
+// string parrafo="Repasando Laboratorio";
+// Console.WriteLine(parrafo);
+// char letra='z';
+// Console.WriteLine(letra);//
 
-// Estructura de Repetición
+// // Estructura Selectiva
+// Console.Write("Ingrese su nombre: ");
+// string Nombre=Console.ReadLine();
+// if(Nombre=="Jose"){
+//    Console.WriteLine("Bienvenido"+ Nombre);
+// }
+// else if (Nombre!="Jose")
+// {
+// Console.WriteLine("Acceso denegado");
+// }
+//Estructura de Repetición
 /*
 for(int i=0;i<11;i++){
     Console.WriteLine(i);
@@ -40,47 +37,44 @@ while(i<31)
     
     Console.WriteLine(i);
     i++;
-}
-int i=40;
+}*/
+/*int i=40;
 do{
 Console.WriteLine(i);
 i++;
 } while(i<51);*/
 
-/*Funciones*/
-
-Console.WriteLine("ingrese su nombre: ");
-string name=Console.ReadLine();
- static void saludar(string name){
-    Console.WriteLine("Hola Buenas noches: "+ name);
-}
-saludar(name);
-saludar(name);
-saludar(name);
-
+int[]edades_personas=new int[4];
+/* Mostrar por array de uno a uno, sin ciclo de repetición*/
+edades_personas[0]=24;
+edades_personas[1]=97;
+edades_personas[2]=54;
+edades_personas[3]=22;
 /*
-
-Console.WriteLine("Ingrese num: ");
-int num = Int32.Parse(Console.ReadLine());
-
-static string calcular (int num){
-    string mensaje;
-    if (num % 2 == 0){
-        mensaje = "par";
-    }
-    else{
-        mensaje = "impar";
-    }
-    return mensaje;
-}
-string m = calcular(num);
-Console.Write(m);
-
+Console.WriteLine(edades_personas[1]);*/
+/*Ciclo de repetición FOR*/
 /*
-int num1=2,num2=9;
-int resultado;
- int producto(int num1,int num2){
-resultado=num1*num2;
-return resultado;
+for(int i=0; i<edades_personas.Length; i++){
+    Console.WriteLine(edades_personas[i]);
+
+}*/
+/*
+string [] nombres_mascotas = new string [3];
+Console.WriteLine(nombres_mascotas.Length);
+for(int i=0; i<nombres_mascotas.Length; i++){
+    Console.WriteLine("ingrese el nombre de su mascota: " + i);
+    nombres_mascotas[i]=Console.ReadLine();
 }
-Console.WriteLine(producto( num1,num2));*/
+
+for (int i=0; i<nombres_mascotas.Length; i++){
+    Console.WriteLine(nombres_mascotas[i]);
+}
+*/
+
+double [] reales =new double [ 2];
+reales [0]= 1.23;
+reales[1]= 19.58;
+
+foreach(var real  in reales ){
+    Console.WriteLine(real);
+}
